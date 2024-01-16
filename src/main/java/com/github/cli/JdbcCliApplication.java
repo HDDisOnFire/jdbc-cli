aec1c2d2e870027b88037d3cf75f012bf8f055f5
Kiyo <ligy@msn.com>
2024-01-16 23:32:11 +0800

    初始化项目

---
+++ src/main/java/com/github/cli/JdbcCliApplication.java
@@ -0,0 +1,14 @@
+package com.github.cli;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 创建一个初始化的命令应用，方便明天现场调试界面
+ */
+@Slf4j
+public class JdbcCliApplication {
+
+    public static void main(String[] args) {
+        log.info("Hello,Jdbc Cli Application!");
+    }
+}
